82e0027057f98fa51121faf1b455066e486078d6
spiism <sp.iism@gmail.com>
2024-05-29 10:05:13 +1200

    add nearbyJob components

--- components/common/cards/nearby/NearbyJobCard.jsx
+++ components/common/cards/nearby/NearbyJobCard.jsx
@@ -1,14 +1,32 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from "react";
+import styles from "./nearbyjobcard.style";
+import { View, Text, TouchableOpacity, Image } from "react-native";
+import { checkImageURL } from "../../../../utils";
 
-import styles from './nearbyjobcard.style'
-
-const NearbyJobCard = () => {
+const NearbyJobCard = (job, handleNavigate) => {
+  console.log(job);
   return (
-    <View>
-      <Text>NearbyJobCard</Text>
-    </View>
-  )
-}
+    <TouchableOpacity style={styles.container} onPress={() => handleNavigate}>
+      <TouchableOpacity style={styles.logoContainer}>
+        <Image
+          source={{
+            uri: checkImageURL(job.job.employer_logo)
+              ? "https://via.placeholder.com/150"
+              : job.job.employer_logo,
+          }}
+          resizeMode="contain"
+          style={styles.logoImage}
+        />
+      </TouchableOpacity>
 
-export default NearbyJobCard
+      <View style={styles.textContainer}>
+        <Text style={styles.jobName} numberOfLines={1}>
+          {job.job.job_title}
+        </Text>
+        <Text style={styles.jobType}>{job.job.job_employment_jobType}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default NearbyJobCard;
--- components/home/nearby/Nearbyjobs.jsx
+++ components/home/nearby/Nearbyjobs.jsx
@@ -1,12 +1,43 @@
-import React from "react";
-import { View, Text } from "react-native";
-
+import { View, Text, TouchableOpacity, ActivityIndicator } from "react-native";
+import { useRouter } from "expo-router";
 import styles from "./nearbyjobs.style";
+import { COLORS } from "../../../constants";
+import useFetch from "../../../hook/useFetch";
+import NearbyJobCard from "../../common/cards/nearby/NearbyJobCard";
 
 const Nearbyjobs = () => {
+  const router = useRouter();
+  const { data, isLoading, error } = useFetch("search", {
+    query: "Python developer in Texas, USA",
+    page: "1",
+    num_pages: 1,
+  });
+
+  console.log(data);
   return (
-    <View>
-      <Text>Nearbyjobs</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Nearby Jobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Show all</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.cardsContainer}>
+        {isLoading ? (
+          <ActivityIndicator size="large" colors={COLORS.primary} />
+        ) : error ? (
+          <Text>Something went wrong</Text>
+        ) : (
+          data?.data?.map((job) => (
+            <NearbyJobCard
+              job={job}
+              key={`nearby-job-${job?.job_id}`}
+              handleNavigate={() => router.push(`/job-details/${job?.job_id}`)}
+            />
+          ))
+        )}
+      </View>
     </View>
   );
 };
